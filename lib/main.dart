dd4130d856dde98298fe25e7c12383ca99755de0
Saudkhan101581 <saudkhan17218@gmail.com>
2025-07-05 12:26:52 +0500

    created tasbeeh counter app

--- lib/main.dart
+++ lib/main.dart
@@ -1,92 +1,125 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(counterApp());
+  runApp(tasbeehCounterApp());
 }
 
-class counterApp extends StatefulWidget {
-  const counterApp({super.key});
+class tasbeehCounterApp extends StatefulWidget {
+  const tasbeehCounterApp({super.key});
 
   @override
-  State<counterApp> createState() => _counterAppState();
+  State<tasbeehCounterApp> createState() => _tasbeehCounterAppState();
 }
 
-class _counterAppState extends State<counterApp> {
+class _tasbeehCounterAppState extends State<tasbeehCounterApp> {
   int count = 0;
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.amber,
-          title: Text(
-            'Counter App',
-            style: TextStyle(
-              color: Colors.black,
-              fontWeight: FontWeight.bold,
-              fontSize: 30,
-            ),
-          ),
-        ),
-        floatingActionButton: Column(
-          mainAxisAlignment: MainAxisAlignment.end,
+        body: Stack(
           children: [
-            FloatingActionButton(
-              onPressed: () {
-                setState(() {
-                  count++;
-                });
-              },
-              backgroundColor: Colors.amber,
-              child: Icon(Icons.add, color: Colors.black),
+            Container(
+              height: double.infinity,
+              width: double.infinity,
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage('assets/tasbeeh.jpg'),
+                  fit: BoxFit.cover,
+                ),
+              ),
             ),
-            SizedBox(height: 10),
-            FloatingActionButton(
-              onPressed: () {
-                setState(() {
-                  if (count != 0) {
-                    count--;
-                  } else {
-                    count == 0;
-                  }
-                });
-              },
-              backgroundColor: Colors.amber,
-              child: Icon(Icons.minimize_sharp, color: Colors.black),
+            Padding(
+              padding: const EdgeInsets.only(top: 300, left: 200),
+              child: Container(
+                height: 50,
+                width: 100,
+                alignment: Alignment.center,
+                //color: Colors.black,
+                child: Text(
+                  '$count',
+                  style: TextStyle(
+                    color: Colors.amberAccent,
+                    fontSize: 35,
+                    fontWeight: FontWeight.bold,
+                    fontFamily: 'RobotoMono',
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+              ),
             ),
-            SizedBox(height: 10),
-            FloatingActionButton(
-              onPressed: () {
-                setState(() {
-                  count = 0;
-                });
-              },
-              backgroundColor: Colors.amber,
-              child: Icon(Icons.refresh, color: Colors.black,),
+
+            Padding(
+              padding: EdgeInsets.only(top: 485, left: 215),
+              child: IconButton(
+                onPressed: () {
+                  setState(() {
+                    count++;
+                  });
+                },
+                icon: Icon(Icons.add, size: 55, color: Colors.amber),
+              ),
             ),
-          ],
-        ),
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.end,
-          children: [
-            Center(
+            Padding(
+              padding: EdgeInsets.only(top: 390, left: 305),
+              child: IconButton(
+                onPressed: () {
+                  setState(() {
+                    count = 0;
+                  });
+                },
+                icon: Icon(Icons.refresh, size: 35, color: Colors.amber),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.only(top: 390, left: 142),
+              child: IconButton(
+                onPressed: () {
+                  setState(() {
+                    if (count != 0) {
+                      count--;
+                    } else {
+                      count == 0;
+                    }
+                  });
+                },
+                icon: Icon(Icons.remove, size: 35, color: Colors.amber),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 360, left: 140),
               child: Text(
-                '$count',
+                'Minimize',
                 style: TextStyle(
-                  color: Colors.black,
-                  fontSize: 35,
+                  color: Colors.white,
                   fontWeight: FontWeight.bold,
                 ),
               ),
             ),
-            SizedBox(height: 300,),
-            Text('Developed By SAUD KHAN',style: TextStyle(color: Colors.black,fontSize: 20,fontWeight: FontWeight.bold),),
-            SizedBox(height: 30,)
-
+            Padding(
+              padding: const EdgeInsets.only(top: 360, left: 313),
+              child: Text(
+                'Reset',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 630, left: 90),
+              child: Text(
+                'Developed By SAUD KHAN',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 30,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
           ],
         ),
-        
       ),
     );
   }
